dd60537fc742fe8ac5a7e81708279bf781fd3307
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-09-23 15:12:30 +0200

    Lektion 01

--- Lektion-01/variabler.php
+++ Lektion-01/variabler.php
@@ -25,8 +25,43 @@ define('SITE_TITLE' , "Nackademin Yrkeshögskola");
         
         // Utskift av en variabel
         echo $name;
+        echo "<br>";
+
+        // Visa typ med hjälp av funktionen var_dump
+        var_dump($name); // string
+        echo "<br>";
+        echo is_string($name); // 1
+        echo "<br>";
+
+        var_dump($age); // int
+        echo "<br>";
+        echo is_string($age); // tomt resultat
+        echo "<br>";
+        echo is_int($age); // 1 (sant)
+        echo "<br>";
+
+        $price = 10.50;
+        var_dump($price); // float
+        echo "<br>";
+        echo is_numeric($price); // 1
+        echo "<br>"; 
+        $test = "170"; // OBS! Detta är en sträng
+        echo is_int($test);  // tomt resultat
+        echo "<br>";
+        echo is_numeric($test); // 1
 
         echo "<br>";
+
+        $married = true;
+        var_dump($married);  // bool
+
+        echo "<br>";
+
+        $demo_variabel;
+        var_dump($demo_variabel);  // NULL
+
+        echo "<br>";
+
 
         // Utskift av en konstant
         echo SITE_TITLE;
@@ -40,13 +75,15 @@ define('SITE_TITLE' , "Nackademin Yrkeshögskola");
         echo "<br>";
 
         // Magiska konstanter
-        echo "Rad: " . __LINE__ . "<br>";
-        echo "Rad: " . __LINE__ . "<br>";
+        echo "Rad: " . __LINE__ . '<br>';
+        echo "Rad: " . __LINE__ . '<br>';
         echo "Rad: " . __LINE__ . "<br>";
         echo "Mapp: " . __DIR__ . "<br>";
         echo "Filen ligger här: " . __FILE__ . "<br>";
-        
 
+        // Strängar
+        echo "<h2>$name är $age gammal!</h2>";
+        echo '<h2>$name är $age gammal!</h2>';
 
     ?>
     <hr>
